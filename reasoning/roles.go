00031a6c66a9ea0c9680177505083fbabdf98286
MarkNahabedian <naha@mit.edu>
2018-06-25 11:26:34 -0400

    New implementation of roles.  Previous wouldn;t have worked due to language limitations (no first class tyupes).

--- reasoning/roles.go
+++ reasoning/roles.go
@@ -1,59 +1,47 @@
-// For each role we define a function named by that role which takes a
-// formation as argument and retuirns a sloice of Dancer.
 package reasoning
 
 import "squaredance/geometry"
 import "squaredance/dancer"
 
-type Role func(Formation) []dancer.Dancer
+
+type Role interface {
+	// Name returns the name of the Role.
+	// By convention, Role names are singular, so "Head" rather than "Heads".
+	Name() string
+
+	// MeaningfulTo returns true if the Formation applies some meaning to the Role.
+	MeaningfulTo(Formation) bool
+
+	// Dancers returns the dancers from the Formation that fit this Role.
+	Dancers(Formation) []dancer.Dancer
+}
 
 var Roles []Role
 
-// Some roles depend on the formation.  For these, the relevant
-// formation defines a method of the same name as the role function which
-// the role function calls for its result.  For these, we also define an
-// interface "Has<role>" which has that method as its only member.
-
-// Other roles depend only on the dancers themselves.
-
-func OriginalHead(f Formation) []dancer.Dancer {
-	result := []dancer.Dancer{}
-	for _, d := range f.Dancers() {
-		cn := d.CoupleNumber()
-		if cn == 1 || cn == 3 {
-			result = append(result, d)
+func LookupRole(name string) Role {
+	for _, r := range Roles {
+		if name == r.Name() {
+			return r
 		}
 	}
-	return result
+	return nil
 }
 
-func OriginalSide(f Formation) []dancer.Dancer {
-	result := []dancer.Dancer{}
-	for _, d := range f.Dancers() {
-		cn := d.CoupleNumber()
-		if cn == 2 || cn == 4 {
-			result = append(result, d)
-		}
-	}
-	return result
+
+// ubiquitousRole is used to implement any Role that is relevant to all formations.
+type ubiquitousRole struct {
+	name string
+	filter func(dancer.Dancer) bool
 }
 
-func CurrentHead(f Formation) []dancer.Dancer {
-	result := []dancer.Dancer{}
-	for _, d := range f.Dancers() {
-		dd := d.Direction()
-		if dd.Equal(geometry.Direction(0)) || dd.Equal(geometry.Direction(0).Opposite()) {
-			result = append(result, d)
-		}
-	}
-	return result
-}
+func (r *ubiquitousRole) Name() string { return r.name }
 
-func CurrentSide(f Formation) []dancer.Dancer {
+func (r *ubiquitousRole) MeaningfulTo(Formation) bool { return true }
+
+func (r *ubiquitousRole) Dancers(f Formation) []dancer.Dancer {
 	result := []dancer.Dancer{}
 	for _, d := range f.Dancers() {
-		dd := d.Direction()
-		if dd.Equal(geometry.Direction(0).QuarterRight()) || dd.Equal(geometry.Direction(0).QuarterLeft()) {
+		if r.filter(d) {
 			result = append(result, d)
 		}
 	}
@@ -61,5 +49,44 @@ func CurrentSide(f Formation) []dancer.Dancer {
 }
 
 func init() {
-	Roles = append(Roles, OriginalHead, OriginalSide, CurrentHead, CurrentSide)
+	Roles = append(Roles,
+		&ubiquitousRole {
+			name: "OriginalHeads",
+			filter: func(d dancer.Dancer) bool {
+				cn := d.CoupleNumber()
+				return cn & 1 == 1
+			},
+		},
+		&ubiquitousRole {
+			name: "OriginalSides",
+			filter: func(d dancer.Dancer) bool {
+				cn := d.CoupleNumber()
+				return cn & 1 == 0
+			},
+		},
+		&ubiquitousRole {
+			name: "CurrentHeads",
+			filter: func(d dancer.Dancer) bool {
+				dd := d.Direction()
+				// Only works for 8 dancer sets.
+				return (dd.Equal(geometry.Direction(0)) || dd.Equal(geometry.Direction(0).Opposite()))
+			},
+		},
+		&ubiquitousRole {
+			name: "CurrentSides",
+			filter: func(d dancer.Dancer) bool {
+				dd := d.Direction()
+				// Only works for 8 dancer sets.
+				return (dd.Equal(geometry.Direction(0).QuarterRight()) ||
+					dd.Equal(geometry.Direction(0).QuarterLeft()))
+			},
+		},
+	)
 }
+
+
+// Some roles depend on the formation.  For these, the relevant
+// formation defines a method of the same name as the role which
+// the Role's Dancers method calls for its result.  For these, we
+// also define an interface "Has<role>" which has that method as
+// its only member.
