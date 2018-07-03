08a309e74afd084db457cba1d998829c5e68fead
MarkNahabedian <naha@mit.edu>
2018-07-03 16:09:51 -0400

    Code simplification.  Runs cleanly, but there are no meaningful test cases yet.

--- reasoning/reasoning_test.go
+++ reasoning/reasoning_test.go
@@ -3,23 +3,46 @@ package reasoning
 import "testing"
 import "squaredance/dancer"
 import "goshua/rete"
+import "goshua/rete/rule_compiler/runtime"
+
+
+func loadAllRules(root rete.Node) {
+	for _, rule := range runtime.AllRules {
+		rule.Inserter()(root)
+	}
+}
+
+func bufferAllTypes(root rete.Node) {
+	// Make sure the assertions get buffered so we can dump them:
+	rete.Walk(root, func(n rete.Node) {
+		if _, ok := n.(*rete.TypeTestNode); ok {
+			_ = rete.GetBuffered(n)
+		}
+	})
+}
+
+func showAllAssertions(t *testing.T, root rete.Node) {
+	rete.Walk(root, func(n rete.Node) {
+		if n, ok := n.(*rete.BufferNode); ok {
+			t.Logf("Dump: %s", n.Label())
+			c := n.GetCursor()
+			for item, present := c.Next(); present; item, present = c.Next() {
+				t.Logf("    %#v", item)
+			}
+		}
+	})
+}
+
 
 func init() {
 	rete.EnsureTypeTestRegistered("Couple", func(i interface{}) bool { _, ok := i.(Couple); return ok })
 	rete.EnsureTypeTestRegistered("MiniWave", func(i interface{}) bool { _, ok := i.(MiniWave); return ok })
 }
 
-func TestCouple(t *testing.T) {
+func TestShowFullRete(t *testing.T) {
 	root_node := rete.MakeRootNode()
-	SetHasDancersRule(root_node)
-	PairOfDancersRule(root_node)
-	GeneralizedCoupleRule(root_node)
-	MiniWaveRule(root_node)
-
-	// Make sure the assertions get buffered so we can dump them:
-	rete.GetBuffered(rete.GetTypeTestNode(root_node, "Couple"))
-	rete.GetBuffered(rete.GetTypeTestNode(root_node, "MiniWave"))
-
+	loadAllRules(root_node)
+	bufferAllTypes(root_node)
 	// Show the rete
 	rete.Walk(root_node, func(n rete.Node) {
 		t.Logf("node %T %s", n, n.Label())
@@ -28,21 +51,25 @@ func TestCouple(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	rete.WriteGraphvizFile(graph, "TestCouple_rete.dot")
+	rete.WriteGraphvizFile(graph, "formations_rete.dot")
+}
+
+
+func TestCouple(t *testing.T) {
+	root_node := rete.MakeRootNode()
+	loadAllRules(root_node)
+	bufferAllTypes(root_node)
 
 	set := dancer.NewSquaredSet(4)
 	root_node.Receive(set)
 
-	rete.Walk(root_node, func(n rete.Node) {
-		if n, ok := n.(*rete.BufferNode); ok {
-			t.Logf("Dump: %s", n.Label())
-			c := n.GetCursor()
-			for item, present := c.Next(); present; item, present = c.Next() {
-				t.Logf("    %v", item)
-			}
-		}
-	})
-
+	showAllAssertions(t, root_node)
 	t.Errorf("foo")
+}
+
+func TestTwoFacedLines(t *testing.T) {
+	root_node := rete.MakeRootNode()
+	loadAllRules(root_node)
+
 }
 
