ad9172b4715c7706d56587ad6196513bdbace6bb
MarkNahabedian <naha@mit.edu>
2018-07-03 11:02:28 -0400

    Start defining four dancer formations.

--- reasoning/make.go
+++ reasoning/make.go
@@ -1,4 +1,7 @@
-// This file contains no code.  It is just a repository og go:generate directives.
+// This file contains no code.  It is just a repository of go:generate directives.
+//
+//  go generate squaredance/reasoning
+//
 package reasoning
 
 // Support for square dance Roles
@@ -6,8 +9,8 @@ package reasoning
 //go:generate generate_roles
 
 // Compiling rules
-//go:generate rule_compiler set.rules two_dancers.rules 
+//go:generate rule_compiler set.rules two_dancers.rules four_dancers.rules
 
 // Expanding formation boilerplate code
 //go:generate go build squaredance/reasoning/formation_expander
-//go:generate formation_expander two_dancers.go
+//go:generate formation_expander two_dancers.go four_dancers.go
