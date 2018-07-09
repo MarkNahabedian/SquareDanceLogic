2a1ae7d25cef274148dd16b1ed91e8eb12317f9c
MarkNahabedian <naha@mit.edu>
2018-07-09 11:21:14 -0400

    Record dancers's position and direction over time.

---
+++ timeline/timeline.go
@@ -0,0 +1,92 @@
+// Package timeline records Dancer position and direction over time.
+package timeline
+
+import "squaredance/geometry"
+import "squaredance/dancer"
+
+// Time represents a time for the purpose of a Timeline.  Applications
+// might just increment it for successive snapshots or might use it to
+// correspond to beats in the music that the dancers are dancing to.
+type Time int
+
+// DancerSnapshot encapsulates the Position and Direction of a
+// specific Dancer at a specific Time.
+type DancerSnapshot interface {
+	DancerSnapshot()
+	Time() Time
+	Dancer() dancer.Dancer
+	Position() geometry.Position
+	Direction() geometry.Direction
+}
+
+type implDancerSnapshot struct {
+	time Time
+	dancer dancer.Dancer
+	position geometry.Position
+	direction geometry.Direction
+}
+
+func (ds *implDancerSnapshot) DancerSnapshot() {}
+func (ds *implDancerSnapshot) Time() Time { return ds.time }
+func (ds *implDancerSnapshot) Dancer() dancer.Dancer { return ds.dancer }
+func (ds *implDancerSnapshot) Position() geometry.Position { return ds.position }
+func (ds *implDancerSnapshot) Direction() geometry.Direction { return ds.direction }
+
+
+// Timeline is used to record the positions and directions of a group
+// of Dancers over time.
+type Timeline interface {
+	Timeline()
+	Dancers() dancer.Dancers
+	MostRecent() Time
+	FindSnapshot(dancer.Dancer, Time) DancerSnapshot
+	MakeSnapshot(Time)
+}
+
+type implTimeline struct {
+	dancers dancer.Dancers
+	mostRecent Time
+	snapshots []DancerSnapshot
+}
+
+func (tl *implTimeline) Timeline() {}
+func (tl *implTimeline) Dancers() dancer.Dancers { return tl.dancers }
+func (tl *implTimeline) MostRecent() Time { return tl.mostRecent }
+
+// FindSnapshot looks through the recorded DancerSnapshots for this
+// Timeline for one that matches the specified Dancer and Time.
+func (tl *implTimeline) FindSnapshot(d dancer.Dancer, time Time) DancerSnapshot {
+	for _, s := range tl.snapshots {
+		if d == s.Dancer() && time == s.Time() {
+			return s
+		}
+	}
+	return nil
+}
+
+// MakeSnapshot records DancerSnapshots for the tracked dancers
+// labeled with the specified Time.
+func (tl *implTimeline) MakeSnapshot(time Time) {
+	for _, d := range tl.Dancers() {
+		s := &implDancerSnapshot {
+			time: time,
+			dancer: d,
+			position: d.Position(),
+			direction: d.Direction(),
+		}
+		tl.snapshots = append(tl.snapshots, s)
+	}
+	if time > tl.mostRecent {
+		tl.mostRecent = time
+	}
+}
+
+// NewTimeline returns a new, empty Timeline for tracking the
+// specified dancers.
+func NewTimeline(dancers dancer.Dancers) Timeline {
+	return &implTimeline{
+		dancers: dancers,
+		mostRecent: Time(0),
+		snapshots: []DancerSnapshot{},
+	}
+}
