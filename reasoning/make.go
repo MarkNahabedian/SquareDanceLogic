7bc5778d3f29b41fc65a1ceaaa1569d7609984f8
MarkNahabedian <naha@mit.edu>
2020-09-04 18:09:19 -0400

    Add OUTPUTS comments to list build products.

--- reasoning/make.go
+++ reasoning/make.go
@@ -7,12 +7,21 @@ package reasoning
 // Support for square dance Roles
 //go:generate go build squaredance/reasoning/generate_roles
 //go:generate generate_roles
+// OUTPUTS: generated_roles.go
 
 // Expanding formation boilerplate code
 //go:generate defimpl
+// OUTPUTS impl_*.go
 //go:generate go build squaredance/reasoning/formation_expander
 //go:generate formation_expander two_dancers_rules.go four_dancers_rules.go
+// OUTPUTS: feout_*.go
 
 // Compiling rules
 //go:generate rule_compiler
+// INPUTS: *_rules.go
+// OUTPUTS: *_rules_out.go
+
+// Running unit tests and generating some documentation fioles:
+//go:generate go test squaredance/reasoning
+// OUTPUTS: all_rules.txt, formation_types.txt, formations_rete.dot
 
