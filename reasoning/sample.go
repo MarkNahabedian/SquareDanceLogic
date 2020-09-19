11ed4d4da915532dd39bfe36827b8bbc71b1297f
MarkNahabedian <naha@mit.edu>
2020-09-19 11:09:30 -0400

    Avoid null pointer exception.

--- reasoning/sample.go
+++ reasoning/sample.go
@@ -12,8 +12,14 @@ var formation_sample_constructors = map[reflect.Type] func()Formation{}
 // MakeSampleFormation returns nil or a sample square fance formation
 // of the specified type.
 func MakeSampleFormation(formation_type reflect.Type) Formation {
+	if formation_type.Kind() == reflect.Slice {
+		return nil
+	}
 	if formation_type.Kind() == reflect.Interface {
 		impl := runtime.InterfaceToImpl(formation_type)
+		if impl == nil {
+			return nil
+		}
 		return MakeSampleFormation(impl)
 	}
 	constructor, ok := formation_sample_constructors[formation_type]
