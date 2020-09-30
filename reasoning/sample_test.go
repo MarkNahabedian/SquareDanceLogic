21c071ea8999f5176160c8ab0d6c3cdeeeac2162
MarkNahabedian <naha@mit.edu>
2020-09-29 22:11:21 -0400

    Dumpo rete buffers on failure.

--- reasoning/sample_test.go
+++ reasoning/sample_test.go
@@ -2,12 +2,14 @@ package reasoning
 
 import "testing"
 import "defimpl/runtime"
+import "goshua/rete"
 
 
 // TestMakeSample makes sure that each sample formation returned by
 // MakeSampleFormation matches the rule for that formation.
 func TestMakeSampleFormation(t *testing.T) {
 	runtime.Dump()
+	/*
 	ff := MakeFormationFinder()
 	for bft, bn := range ff.typeToBuffer {
 		t.Logf("Buffer for %s: \t%T", bft, bn)
@@ -15,13 +17,19 @@ func TestMakeSampleFormation(t *testing.T) {
 	for ft, _ := range formation_sample_constructors {
 		t.Logf("Constructor for %s", ft)
 	}
-
+	*/
 	for ft, c := range formation_sample_constructors {
 		sample := c()
-		t.Logf("%s %v", ft, sample)
-		f := FindFormations(sample.Dancers(), ft)
-		if len(f) != 1 {
-			t.Errorf("Expected one formation of type %s", ft)
+		// t.Logf("%s %v", ft, sample)
+		f, ff := FindFormations(sample.Dancers(), ft)
+		if count := len(f); count != 1 {
+			t.Errorf("Expected one formation of type %s, got %d", ft, count)
+			ff.DoAllBuffers(func (bn rete.AbstractBufferNode) {
+				t.Logf("rete Node %s:\n", bn.(rete.Node).Label())
+				bn.DoItems(func(item interface{}) {
+					t.Logf("    %v\n", item)
+				})
+			})
 		}
 	}
 }
